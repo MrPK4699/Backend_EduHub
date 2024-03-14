b523c627f589806d2a6fcd7e005f9e55535cb21a
Pankaj Khatri <4699pankajkhatri@gmail.com>
2024-03-14 17:25:12 +0530

    ds

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('../Models/User');
 const {jwtSecret}= require('../config/config');
       
 
