a47941eef3c5060c4d13fa167727d2b2c5f73dfc
Pankaj Khatri <4699pankajkhatri@gmail.com>
2024-03-14 17:56:47 +0530

    sa

--- index.js
+++ index.js
@@ -13,9 +13,6 @@ const errorHandlers=require('./utils/errorHandlers')
 const authenticateToken= require('./Middlewares/authmiddleware')
 
 const app= express();
-console.log(MongoUrl)
-console.log('process.env.MONGODB_URI');
-console.log(process.env.MONGODB_URI);
 
 mongoose.connect(MongoUrl,  { useNewUrlParser: true, useUnifiedTopology: true })
 .then(()=>{
